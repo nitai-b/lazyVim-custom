fc8f898eb2a6ffbc800c7942b7402d0f8efbc427
nitai-b <nitaibarran5@gmail.com>
2025-10-03 16:24:48 -0400

    my keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,7 +2,9 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set("n", "<C-n>", "", {})
-vim.keymap.set("n", "<C-n>", ":Neotree toggle<CR>", {})
+-- Toggle file explorer with Ctrl+n
+vim.keymap.set("n", "<C-n>", function()
+  Snacks.explorer()
+end, { desc = "Toggle file explorer", silent = true, noremap = true })
 vim.keymap.set("n", "<[q>", ":cprevious<CR>", {})
 vim.keymap.set("n", "<]q>", ":cnext<CR>", {})
